11b3c07b7c2b9189c45d88502d78dc24a147d9fe
Nimfell <44020287+Nimfell@users.noreply.github.com>
2019-02-11 14:27:04 +0300

    Add files via upload

---
+++ HashTableTemplate.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+
+class HashTable
+{
+public:
+  int size;
+  int step;
+  char** slots;
+
+  HashTable(int sz, int stp)
+  {
+    size = sz;
+    step = stp;
+    slots = new char*[size];
+    for (int i = 0; i<size; i++) slots[i] = NULL;
+  }
+
+  int hashFun(char* value)
+  {
+    //int len = value.Length();
+    int index = 0;
+    index = (int)value[0];
+    for (int i = 1; value[i] != 0; i++)          // adder of sumbols
+    {
+      index += (int)value[i];
+    }
+    if (index < 0) index *= -1;          // number modulus
+    while (index >= size)                // if larger that the size
+    {
+      index /= 3;
+    }
+    return index;
+  }
+
+  int seekSlot(char* value)
+  {
+    int index = hashFun(value);
+    for (int i = 0; i < 3; i++)
+    {
+      while (index <= size - 1)
+      {
+        if (slots[index] == NULL)
+        {
+          //slots[index] = value;
+          return index;
+        }
+        index += step;
+      }
+      index = index - size;
+    }
+    for (int i = 0; i <= size; i++)
+    {
+      if (slots[i] == NULL)
+      {
+       // slots[i] = value;
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  int put(char* value)
+  {    
+    int index = seekSlot(value);
+    if (index == -1) return index;
+    slots[index] = value;
+    return index;
+  }
+
+  int find(char* value)
+  {    
+	for (int index = 0; index < size; index++) 
+    {
+		if (slots[index] != NULL)
+		{				
+			for (int i = 0; i < 256; i++)
+			{
+				char slot = *( slots[index]+i );
+				char val  = *( value+i );
+				
+				if ( slot == val && val == 0 ) 
+					return index;				
+
+				if ( slot != val || val == 0)				
+						break;
+			}
+		}
+		else if (value == NULL)
+				return index;
+    }
+    return -1;   
+  }
+};
+
+//====================== TEST ========================
+/*
+int test_hash(HashTable* Table)
+{
+  //HashTable* Table = new HashTable(17, 3);
+  char* value = "It’s better to light a candle than curse the darkness";
+  char* value2 = "It’s";
+  char value3[54] = "It’s better to light a candle than curse the darkness";
+  char* value4 = "darkness";
+
+  char* val_2 = "You can’t make bricks without straw";
+  int step = Table->step;
+  int test = 0;
+
+  // FIND
+  if (Table->find(NULL) != 0) test++;     // checking the filling of the empty table
+                                          // if (Table->find(0) != -1) test++;       // this value is not in the table - don't work
+
+  // PUT
+  int index = Table->hashFun(value);        // index = 6
+  for (int i = 0; i < Table->size - 2; i++) // fill the entire array except for one cell
+  {
+    index = (index <= Table->size - 1) ? index : index - Table->size;
+    if (Table->put(value) != index) test++;
+    index += step;
+  }
+  if (Table->find(value3) != 1) test++;
+  index = (index <= Table->size - 1) ? index : index - Table->size;
+  if (Table->put(value2) != index) test++;   
+  if (Table->find(value2) != index) test++;
+  index += step;  
+  index = (index <= Table->size - 1) ? index : index - Table->size; // the last empty data cell
+  if (Table->put(val_2) != index) test++;
+  if (Table->find(val_2) != index) test++;
+  if (Table->put(value4) != -1) test++;    //the table is full
+  if (Table->find(value4) != -1) test++;
+
+  return test;
+}
+*/
+
+
