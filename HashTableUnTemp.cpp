23d40a439315a9c8cca9b20b00328a30a470bce2
Nimfell <44020287+Nimfell@users.noreply.github.com>
2019-02-14 11:33:41 +0300

    Change with lists

--- HashTableUnTemp.cpp
+++ HashTableUnTemp.cpp
@@ -1,14 +1,14 @@
-
 #include <stdio.h>
 #include <cstdlib>
-//#include <string.h> 
+#include <list>
+using namespace std;
 
 int hashFun_1(int value, int size)
 {   
 	if (value == NULL)
 		return -1;
 
-	int p = 37; // prime number
+	int p = 37; // Prime number
 	int a = 30; // 1 ... ð-1
 	int b = 2;  // 0 ... ð-1
 
@@ -21,7 +21,7 @@ int hashFun_2(int value, int size)
 {    
 	if (value == NULL)
 		return -1;
-	int p = 17; // prime number
+	int p = 17; // Prime number
 	int a = 9;  // 1 ... ð-1
 	int b = 5;  // 0 ... ð-1
 	int index = ( (a*value + b) % p ) % size;
@@ -31,7 +31,7 @@ int hashFun_2(int value, int size)
 int hashFun_3(int value, int size)
 {    
 	if (value == NULL) return -1;
-	int p = 37; // prime number
+	int p = 37; // Prime number
 	int a = 18; // 1 ... ð-1
 	int b = 9;  // 0 ... ð-1
 	int index = ( (a*value + b) % p ) % size;
@@ -40,11 +40,9 @@ int hashFun_3(int value, int size)
 
 typedef int (*HashFunction)(int value, int size);
 
-
 HashFunction _hashFun_1 = & hashFun_1 ;
 HashFunction _hashFun_2 = & hashFun_2 ;
 HashFunction _hashFun_3 = & hashFun_3 ;
-
 
 HashFunction hash_func_arr[3] = 
 {
@@ -58,7 +56,7 @@ class HashTable
 public:
   int size;
   int step;
-  int* slots;  
+  list<int>* slots;  
   HashFunction _hashFun;
   int number_of_collisions;
 
@@ -67,9 +65,7 @@ public:
     size = sz;
     step = stp;
 	number_of_collisions = 0;
-    slots = new int[size];
-    for (int i = 0; i < size; i++) slots[i] = NULL;
-
+    slots = new list<int>[size];
 	_hashFun = hash_func_arr[ rand() % 3 ]; // arg = 0 by 2	
   }
 
@@ -84,90 +80,53 @@ public:
 
   int seekSlot(int value)
   {
-    int index = hashFun(value);
-
-    for (int i = 0; i < 3; i++)
-    {
-      while (index <= size - 1)
-      {
-        if (slots[index] == NULL)
-        {          
-          return index;
-        }
-		number_of_collisions++;
-        index += step;
-      }
-      index = index - size;
-    }
-	
-    for (int i = 0; i < size; i++)		// slot by slot
-    {
-      if (slots[i] == NULL)
-      {      
-        return i;
-      }
-    }
-    return -1;
+    int index = hashFun(value);         
+          return index;  
   }
   
   int put(int value)
   {    
     int index = seekSlot(value);	// get empty slot index
     if (index == -1) return index;	// if no empty slots
-    slots[index] = value;			
+    
+	slots[index].push_back(value);			
     return index;
   }
 
   int find(int value)
   { 
 	int index = hashFun(value);						//get index
-	if (index == -1) 
-		return -1;
+	list<int> List = slots[index];
 
-    for (int i = 0; i < 3; i++)						// 3 times 
-    {
-      while (index <= size - 1)						// table limit
-      {	
-		if ( slots[index] == value)				// compare
-		{
-			return index;						// if equal
-		}
-        index += step;								// go to next slot  
-      }
-      index = index - size;							// reaching limit			
-    }
-
-    for (int index = 0; index < size; index++)		// slot by slot
-    {	
-		if ( slots[index] == value)				// compare
-		{
-			return index;						// if equal
-		}		
-    }
-    return -1;
+	for (auto i = List.begin(); i != List.end(); ++i)
+	{		
+		if (*i == value)
+			return index;		
+	}
+	return -1;
   }
 };
-
+/*
 //====================== TEST ========================
 int test_hash(HashTable* Table)
 {
-  int test = 0;
-  int t = 0;    
-  for (int i = 0; i <= Table->size; i++) // fill the entire array 
-  {    
-	t++;	
-    Table->put(t);
-  }
-
-  if (Table->put(88) != -1) test++;    //the table is full
-  if (Table->find(88) != -1) test++;
-  if (Table->find(1) != Table->hashFun(1) ) test++;
-  return test;
+	int test = 0;
+	int t = 0;    
+	for (int i = 0; i <= 100; i++) // fill the entire array 
+	{    
+		t++;	
+		Table->put(t);
+	}
+	if (Table->find(1) != Table->hashFun(1) ) test++;
+	if (Table->find(38) != Table->hashFun(38) ) test++;
+	return test;
 }
 
 int main()
 {
-	HashTable* Table = new HashTable(11, 3);	
-	test_hash(Table);		
+	//vector <int> v;
+	HashTable* Table = new HashTable(17, 3);	
+	test_hash(Table);			
 	return 0;
 }
+*/
